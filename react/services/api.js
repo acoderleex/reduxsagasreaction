fc5f7c24396eb30f37e058887739b29b15b632f1
xf.li@8win.com <xf.li@8win.com>
2017-04-11 18:50:35 +0800

    optimize code

--- react/services/api.js
+++ react/services/api.js
@@ -1,37 +1,31 @@
 'use strict';
 import request from '../utils/request';
 
-import {
-   API_ROOT,
-} from '../configs';
+import {API_ROOT} from '../configs';
 
 const DEFAULT_SORTTYPE = 'all';
 const DEFAULT_PAGE = 1;
 const DEFAULT_PAGESIZE = 20;
 
-
-function callApi(endPoint,options){
-  return request(`${API_ROOT}${endPoint}`,options);
+function callApi(endPoint, options) {
+    return request(`${API_ROOT}${endPoint}`, options);
 }
-
 
 export default {
 
-  fetchTopicList: ({
-      boardId,
-      sortType = DEFAULT_SORTTYPE,
-      page = DEFAULT_PAGE,
-      pageSize = DEFAULT_PAGESIZE
-  }) => {
-    return callApi(`forum/topiclist&boardId=${boardId}&sortby=${sortType}&page=${page}&pageSize=${pageSize}`);
-  },
-  fetchForumList:({
-    boardId
-  })=>{
-    let url = 'forum/forumlist';
-    if (boardId && boardId !== 'all') {
-      url += `&fid=${boardId}`;
+    fetchTopicList : ({
+        boardId,
+        sortType = DEFAULT_SORTTYPE,
+        page = DEFAULT_PAGE,
+        pageSize = DEFAULT_PAGESIZE
+    }) => {
+        return callApi(`forum/topiclist&boardId=${boardId}&sortby=${sortType}&page=${page}&pageSize=${pageSize}`);
+    },
+    fetchForumList : ({boardId}) => {
+        let url = 'forum/forumlist';
+        if (boardId && boardId !== 'all') {
+            url += `&fid=${boardId}`;
+        }
+        return callApi(url);
     }
-    return callApi(url);
-  }
 };
--- react/store/index.js
+++ react/store/index.js
@@ -1,21 +1,13 @@
 'use strict';
-import { createStore,applyMiddleware, compose } from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import rootReducer from '../reducers';
 const sagaMiddleware = createSagaMiddleware();
 
-export default function configureStore(initialState={}) {
-  const middlewares =[
-    sagaMiddleware
-  ];
-  const enhancers = [
-    applyMiddleware(...middlewares)
-  ];
-  const store =createStore(
-    rootReducer,
-    initialState,
-    compose(...enhancers)
-  );
-  store.runSaga = sagaMiddleware.run;
-  return store;
+export default function configureStore(initialState = {}) {
+    const middlewares = [sagaMiddleware];
+    const enhancers = [applyMiddleware(...middlewares)];
+    const store = createStore(rootReducer, initialState, compose(...enhancers));
+    store.runSaga = sagaMiddleware.run;
+    return store;
 }
